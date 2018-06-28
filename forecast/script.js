aa64bdc0f1ddc637fc9d7184fa3c0e2a370a2d25
dmi3evaa <40640436+dmi3evaa@users.noreply.github.com>
2018-06-28 23:18:54 +0300

    Divided weatherURITail

--- forecast/script.js
+++ forecast/script.js
@@ -7,8 +7,10 @@ const CityId = {
     LondonId: "2643743",
     ZagrebId: "3337532",
 }
-var weatherURI = "http://api.openweathermap.org/data/2.5/weather?id=";
-var weatherURITail = "&APPID=93a5a6e4650fcb24135b3d5476ad86ff&mode=html";
+var myAPIKey = "93a5a6e4650fcb24135b3d5476ad86ff";
+var weatherURI = "https://api.openweathermap.org/data/2.5/weather?id=";
+var weatherURIKeyRequest = "&APPID=";
+var weatherURITail = "&mode=html";
 var loadCityForecast = function(element) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
@@ -19,15 +21,15 @@ var loadCityForecast = function(element) {
     switch (element.options[element.selectedIndex].value)
 	{
 		case "SPb":
-			xmlHttp.open( "GET", weatherURI+CityId.SPbId+weatherURITail, false );
+			xmlHttp.open( "GET", weatherURI + CityId.SPbId + weatherURIKeyRequest + myAPIKey + weatherURITail, false );
 		break;
 	
 		case "Lon":
-			xmlHttp.open( "GET", weatherURI+CityId.LondonId+weatherURITail, false );
+			xmlHttp.open( "GET", weatherURI + CityId.LondonId + weatherURIKeyRequest + myAPIKey + weatherURITail, false );
 		break;
 	
 		case "Zag":
-			xmlHttp.open( "GET", weatherURI+CityId.ZagrebId+weatherURITail, false );
+			xmlHttp.open( "GET", weatherURI + CityId.ZagrebId + weatherURIKeyRequest + myAPIKey + weatherURITail, false );
 		break;
 	
 		default:
